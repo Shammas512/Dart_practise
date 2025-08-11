ddb8f0b6a8f77eae88bebd34655b1f138d576e3e
Shammas512 <shammasasif7@example.com>
2025-08-11 18:59:58 +0500

    Fixed Bugs of 0 and 1

--- les.dart
+++ les.dart
@@ -6,12 +6,17 @@ void main() {
   int ent = int.parse(entries!);
   for (var i = 0; i < ent; i++) {
     String? numbe = stdin.readLineSync();
+
     int number = int.parse(numbe!);
+    if (number < 0 || number == 0) {
+      print("Please enter a valid number");
+    }
     int facto = 1;
     for (var iw = 1; iw < number + 1; iw++) {
       facto = facto * iw;
     }
+    print(facto);
     my_list.add(facto);
-    print(my_list);
   }
+  print(my_list);
 }
