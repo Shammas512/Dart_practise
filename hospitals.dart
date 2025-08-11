5b88acbde35a09ada5cf46fd90639773afaa21e3
Shammas512 <shammasasif7@example.com>
2025-08-11 20:07:01 +0500

    Add some logics

--- hospitals.dart
+++ hospitals.dart
@@ -8,16 +8,33 @@ void main() {
 }
 
 addpatient() {
+  List doctors = ["Aslam khan", "Atif Jawed", "Saleem"];
   List a = [];
   String? entries = stdin.readLineSync();
   for (var i = 0; i < int.parse(entries.toString()); i++) {
+    String docname = "";
     print("Enter Your name");
     String? name = stdin.readLineSync();
     print("Enter Your diesease");
     String? diesease = stdin.readLineSync();
+    print("select a doctors by number 1,2,3");
+    int count = 1;
+     for (var air in doctors) {
+      print("$count $air");
+      count=count+1;
+    }
+    String? Enter = stdin.readLineSync().toString();
+    if (Enter == "1") {
+      docname = doctors[0];
+    } else if (Enter == "2") {
+      docname = doctors[1];
+    } else {
+      docname = doctors[2];
+    }
     a.add({
       "name": name,
       "diesease": diesease,
+      "doctorname" : docname
     });
   }
   return a;
